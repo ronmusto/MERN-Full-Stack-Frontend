aa31e6f40ed6d03e854f7ff0486e967de3e40059
ronmusto <83174514+ronmusto@users.noreply.github.com>
2023-08-15 22:58:50 -0400

    Hooked up user info to database
    User info is now displayed from databse according to who is logged in

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,25 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { UserContext } from '../UserContext';
 import styles from '../CSS/Account.module.css';
 
 const Account = () => {
+  const { user } = useContext(UserContext);
   const [userData, setUserData] = useState({});
   const [bookedVacations, setBookedVacations] = useState([]);
 
+  const fetchAccountData = () => {
+    console.log('user info', user)
+    if (!user._id) {
+      console.error('User or User ID is not available');
+      return;
+    }
+
+    fetch(`http://localhost:4200/user-data/${user._id}`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => setUserData(data))
+      .catch(err => console.error('Error fetching specified user data', err));
+  };
+
   useEffect(() => {
-    const mockUserData = {
-      username: "JohnDoe",
-      email: "john.doe@example.com"
-    };
     const mockBookedVacations = [
       { id: 1, destination: "Paris", description: "5 days in Paris" },
       { id: 2, destination: "New York", description: "7 days in NYC" },
     ];
     
-    setUserData(mockUserData);
+    fetchAccountData();
     setBookedVacations(mockBookedVacations);
-  }, []);
+  }, [user && user._id]);
 
   return (
+    <UserContext.Provider value={user}>
     <div className={styles.accountContainer}>
       <div className={styles.accountDetails}>
         <h2>{userData.username}</h2>
@@ -35,6 +52,7 @@ const Account = () => {
         ))}
       </div>
     </div>
+    </UserContext.Provider>
   );
 };
 
