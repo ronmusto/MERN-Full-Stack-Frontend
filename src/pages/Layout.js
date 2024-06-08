8f3021b7e5ddefb72b0af22b576ca5fe059b2bb8
ronmusto <83174514+ronmusto@users.noreply.github.com>
2024-06-08 16:46:02 -0400

    changed up CSS for navbar
    remodeled design of navbar

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,29 +1,32 @@
-import '../CSS/bootstrap.css';
+import LayoutStyles from '../CSS/Layout.module.css'; // Import as LayoutStyles
 import React from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Layout = () => {
-
   return (
     <div>
-      <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="/">Home</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-            <Nav.Link as={Link} to="/AI">House AI</Nav.Link>
-            <Nav.Link as={Link} to="/dashboard">Data</Nav.Link>
-            <Nav.Link as={Link} to="/travel">Travel</Nav.Link>
-            <Nav.Link as={Link} to="/account">Account</Nav.Link>
-            <Nav.Link as={Link} to="/login">Login</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+      <Navbar 
+        className={LayoutStyles.layoutHeader} 
+        bg="dark" variant="dark" expand="lg"
+      >
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/">Home</Navbar.Brand> 
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" className="justify-content-center">
+            <Nav className={LayoutStyles.layoutNavList}>
+              <Nav.Link as={Link} to="/contact" className={LayoutStyles.layoutNavLink}>Contact</Nav.Link>
+              <Nav.Link as={Link} to="/dashboard" className={LayoutStyles.layoutNavLink}>Data</Nav.Link>
+              <Nav.Link as={Link} to="/travel" className={LayoutStyles.layoutNavLink}>Travel</Nav.Link>
+              <Nav.Link as={Link} to="/account" className={LayoutStyles.layoutNavLink}>Account</Nav.Link>
+              <Nav.Link as={Link} to="/login" className={LayoutStyles.layoutNavLink}>Login</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
       <Container>
-        <Outlet />
+        <Outlet /> 
       </Container>
     </div>
   );
